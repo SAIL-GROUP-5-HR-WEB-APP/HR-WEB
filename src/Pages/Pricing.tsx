c9f4ffbef6ddde16464e27a1b0c0467b2e86f3c2
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-07-31 11:45:13 +0100

    Just added the html only for now

--- src/Pages/Pricing.tsx
+++ src/Pages/Pricing.tsx
@@ -3,7 +3,99 @@ import React from "react";
 const Pricing = () => {
   return (
     <div>
-      <h1>this is the pricing page</h1>
+      <main>
+        <div>
+          <p>Our Pricing</p>
+          <h1>Pricing plan</h1>
+          <p>Check out our flexible pricing that best fits your plan</p>
+        </div>
+
+        <div>
+          <main>
+            <section>
+              <h4>Basic plan</h4>
+              <h2>$19.99/month</h2>
+              <p>or $180 yearly</p>
+              <div></div>
+            </section>
+
+            <section>
+              <p>Essential features:</p>
+              <li>
+                <ul>Centralized Employee Database Management</ul>
+                <ul>Real-Time Attendance Tracking</ul>
+                <ul>Automated HR Tasks</ul>
+              </li>
+            </section>
+            <section>
+              <p>Perfect for</p>
+              <ul>
+                Small teams and startups looking to streamline basic HR processes.
+              </ul>
+              <div>
+              <button>Subscribe</button>
+              <img src="" alt="arrow" />
+              </div>
+            </section>
+          </main>
+
+          <main>
+            <section>
+              <h4>Basic plan</h4>
+              <h2>$19.99/month</h2>
+              <p>or $180 yearly</p>
+              <div></div>
+            </section>
+
+            <section>
+              <p>Essential features:</p>
+              <li>
+                <ul>Centralized Employee Database Management</ul>
+                <ul>Real-Time Attendance Tracking</ul>
+                <ul>Automated HR Tasks</ul>
+              </li>
+            </section>
+            <section>
+              <p>Perfect for</p>
+              <ul>
+                Small teams and startups looking to streamline basic HR processes.
+              </ul>
+              <div>
+              <button>Subscribe</button>
+              <img src="" alt="arrow" />
+              </div>
+            </section>
+          </main>
+
+          <main>
+            <section>
+              <h4>Basic plan</h4>
+              <h2>$19.99/month</h2>
+              <p>or $180 yearly</p>
+              <div></div>
+            </section>
+
+            <section>
+              <p>Essential features:</p>
+              <li>
+                <ul>Centralized Employee Database Management</ul>
+                <ul>Real-Time Attendance Tracking</ul>
+                <ul>Automated HR Tasks</ul>
+              </li>
+            </section>
+            <section>
+              <p>Perfect for</p>
+              <ul>
+                Small teams and startups looking to streamline basic HR processes.
+              </ul>
+              <div>
+              <button>Subscribe</button>
+              <img src="" alt="arrow" />
+              </div>
+            </section>
+          </main>
+        </div>
+      </main>
     </div>
   );
 };
